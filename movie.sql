c79b912c8a73115feab57dedfd6dcedf3e665d3f
Vinoth124 <108538980+Vinoth124@users.noreply.github.com>
2022-07-01 21:08:00 +0530

    Add files via upload

---
+++ movie.sql
@@ -0,0 +1,5 @@
+create table movie(movie_name varchar(20),lead_actor_name varchar(20),actress_name varchar(20),year_of_releas INTEGER,director_name varchar(20))
+insert into movie values("Annaatthe","Rajinikanth","Nayanthara",2021,"Siva");
+insert into movie values("Sivaji","Rajinikanth","Shreya",2009,"Shankar");
+insert into movie values("Petta","Rajinikanth","Simran",2019,"Karthiksubbaraj");
+insert into movie values("Enthiran","Rajinikanth","Aishwaryaraj",2010,"Shankar");
